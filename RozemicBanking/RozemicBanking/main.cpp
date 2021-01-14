87f1394ddd1b8168a45e10ce56680f8256036f93
waraidev <wileye92@gmail.com>
2021-01-14 14:44:36 -0500

    Origin Commit

---
+++ RozemicBanking/RozemicBanking/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RozemicBanking
+//
+//  Created by Eric Wiley on 1/14/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
