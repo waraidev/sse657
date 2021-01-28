cc45f8c1aea6119d3c82a206a6502b0d9b4c60f3
Dixon <59415488+radixon@users.noreply.github.com>
2021-01-27 21:36:13 -0500

    Home Info Function
    Creating a function in which users can input Home information.  This is simply to test ability to input information.  This will be the basis for inputting information into a SQL or csv file.

--- RozemicBanking/RozemicBanking/main.cpp
+++ RozemicBanking/RozemicBanking/main.cpp
@@ -5,12 +5,71 @@
 //  Created by Eric Wiley on 1/14/21.
 //
 
-#include <iostream>
+	#include <iostream>
+	#include <string>
+	#include <array>
+	/***************************************************************
+	<array> was chosen due to the input requirements being the same
+	for all users.
+	****************************************************************/
+	
+	
+	std::array<std::string,4> customerHome(std::string firstname="N/A",std::string lastname="N/A",std::string address="N/A",std::string zip="N/A" )
+	{
+		/*************************************************************
+		This function will be used to input user information.
+		Defaulting to N/A will allow searches for incomplete entries.
+		**************************************************************/
+		std::string input1;
+		if (firstname == "N/A")
+		{
+			std::cout << "Enter First Name:\n";
+			std::cin >> input1;
+			firstname = input1;
+		}		
+		
+		
+		if (lastname == "N/A")
+		{
+			std::cout << "Enter Last Name:\n";
+			std::cin >> input1;
+			lastname = input1;
+		}
+		
+		if (address == "N/A")
+		{
+			std::cout << "Enter Address:\n";
+			/***************************************************
+			Use getline to allow input that will be more than a 
+			word long.
+			****************************************************/
+			std::getline(std::cin,input1);
+			std::cin.ignore();
+			address = input1;
+		}
+		
+		if (zip == "N/A")
+		{
+			std::cout << "Enter Zip Code:\n";
+			std::cin >> input1;
+			zip = input1;
+		}
+		
+		std::array<std::string,4> newcustomer{firstname,lastname,address,zip};
+		
+		return newcustomer;
+		
+	}
 
-using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    cout << "Hello, World!\n";
-    return 0;
-}
+	int main(void)
+	{
+		std::array<std::string,4> customer;
+		
+		
+		customer = customerHome("John","Doe");
+		for(int i=0; i < 4; i++)
+			std::cout << customer[i] << "\n";
+		
+		return 0;
+	}
