a437a1943a1961a6d42681458acbd66ef2770d94
Emma Anderson <emmagination97@gmail.com>
2021-02-01 17:06:41 -0500

    Moved some functions from the .h file to the .cpp file.

--- RozemicBanking/RozemicBanking/UserAccount.h
+++ RozemicBanking/RozemicBanking/UserAccount.h
@@ -32,36 +32,4 @@ UserAccount::UserAccount() {
     balance = 0;
 }
 
-/**
- * Constructs a user account with a given balance.
- * @param initialBalance The initial balance.
- */
-UserAccount::UserAccount(double initialBalance) {
-    balance = initialBalance;
-}
-
-/**
- * Deposits money into the user's account.
- * @param amount The amount to deposit.
- */
-void UserAccount::deposit(double amount) {
-    balance = balance + amount;
-}
-
-/**
- * Withdraws money into the bank account.
- * @param amount The amount to withdraw.
- */
-void UserAccount::withdraw(double amount) {
-    balance = balance - amount;
-}
-
-/**
- * Gets the current balance of the bank account.
- * @return The current balance.
- */
-double UserAccount::getBalance() {
-    return balance;
-}
-
 #endif //SSE657_USERACCOUNT_H
