2b53035788b2c6d6c6e014cf87ed228316dc90ae
waraidev <wileye92@gmail.com>
2021-02-02 15:34:03 -0500

    #3. #2. Made some file fixes and compiled.
    I moved the files from the folder within a folder and made some small edits.

---
+++ RozemicBanking/CustomerInfo.cpp
@@ -0,0 +1,10 @@
+
+	#include <iostream>
+	#include <string>
+	#include "CustomerInfo.h"
+		
+	int main()
+	{
+		CustomerInfo addcustomer("John", "Doe","3001 Mercer University Dr", "Atlanta", "GA", "30341");
+		std::cout << addcustomer << '\n';
+	}
