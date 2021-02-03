cb937a9e7f18c5dc790287a8711d18108a8f0d64
Dixon <59415488+radixon@users.noreply.github.com>
2021-02-03 14:30:38 -0500

    Update CustomerInfo.h
    Updated to include block /* comment */
    
    Removed unnecessary code.
    
    Formatted for readability.

--- RozemicBanking/CustomerInfo.h
+++ RozemicBanking/CustomerInfo.h
@@ -3,135 +3,124 @@
 
 #include <iostream>
 #include <string>
+
+/*
 #include <thread>
 #include <mutex>
 
-//	mutex mutually;
-//
-//	//Setup For Multithreaded Code
-//	class thread_guard
-//	{
-//		private:
-//			thread& t;
-//		public:
-//			explicit thread_guard(thread& t_) : t(t_) { }
-//
-//			~thread_guard()
-//			{
-//				if(t.joinable())
-//				{
-//					t.join();
-//				}
-//			}
-//
-//			thread_guard(thread_guard const&)=delete;
-//			thread_guard& operator=(thread_guard const&)=delete;
-//
-//	};
+	mutex mutually;
 
-class CustomerInfo {
-private:
-    std::string firstname;
-    std::string lastname;
-    std::string address;
-    std::string city;
-    std::string state;
-    std::string zipcode;
+	//Setup For Multithreaded Code
+	class thread_guard
+	{
+		private:
+			thread& t;
+		public:
+			explicit thread_guard(thread& t_) : t(t_) { }
 
-    //Overloaded stream Operator
-    //friend std::ostream &operator<<(std::ostream &output, CustomerInfo &info);
+			~thread_guard()
+			{
+				if(t.joinable())
+				{
+					t.join();
+				}
+			}
 
-protected:
+			thread_guard(thread_guard const&)=delete;
+			thread_guard& operator=(thread_guard const&)=delete;
 
-public:
+	};
+*/
 
-    //Getters
-    std::string getFirstName();
+    class CustomerInfo 
+    {
+        private:
+            std::string firstname;
+            std::string lastname;
+            std::string address;
+            std::string city;
+            std::string state;
+            std::string zipcode;
 
-    std::string getLastName();
+        protected:
 
-    std::string getAddress();
+        public:
 
-    std::string getCity();
+            //Getters
+            std::string getFirstName();
+            std::string getLastName();
+            std::string getAddress();
+            std::string getCity();
+            std::string getState();
+            std::string getZipCode();
 
-    std::string getState();
+            //Setters
+            std::string setFirstName(std::string first);
+            std::string setLastName(std::string last);
+            std::string setAddress(std::string address);
+            std::string setCity(std::string city);
+            std::string setState(std::string state);
+            std::string setZipCode(std::string zip);
 
-    std::string getZipCode();
+            //Constructor
+            CustomerInfo(std::string firstname = "N/A",
+                         std::string lastname = "N/A",
+                         std::string address = "N/A",
+                         std::string city = "N/A",
+                         std::string state = "N/A",
+                         std::string zip = "N/A");
 
-    //Setters
-    std::string setFirstName(std::string first);
+            //Copy Constructor
+            CustomerInfo(const CustomerInfo &value);
 
-    std::string setLastName(std::string last);
+            //Overloaded Assignment
+            CustomerInfo &operator=(CustomerInfo &unknown);
 
-    std::string setAddress(std::string address);
+            //Destructor
+            ~CustomerInfo();
+        
+            //swap Function
+            void swap(CustomerInfo &lhs, CustomerInfo &rhs) 
+            {
+                using std::swap;
+                swap(lhs.firstname, rhs.firstname);
+                swap(lhs.lastname, rhs.lastname);
+                swap(lhs.address, rhs.address);
+                swap(lhs.city, rhs.city);
+                swap(lhs.state, rhs.state);
+                swap(lhs.zipcode, rhs.zipcode);
+            }
 
-    std::string setCity(std::string city);
-
-    std::string setState(std::string state);
-
-    std::string setZipCode(std::string zip);
+            std::string printInfo();
+    };
 
     //Constructor
-    CustomerInfo(std::string firstname = "N/A",
-                 std::string lastname = "N/A",
-                 std::string address = "N/A",
-                 std::string city = "N/A",
-                 std::string state = "N/A",
-                 std::string zip = "N/A");
-
-    //Copy Constructor
-    CustomerInfo(const CustomerInfo &value);
-
-    //Overloaded Assignment
-    CustomerInfo &operator=(CustomerInfo &unknown);
-
-    //Overloaded stream Operator
-    //friend std::ostream &operator<<(std::ostream &output, CustomerInfo &info);
-
-    //Destructor
-    ~CustomerInfo();
-
-    //Insert
-    void insert(const std::string &words);
-
-    //swap Function
-    void swap(CustomerInfo &lhs, CustomerInfo &rhs) {
-        using std::swap;
-        swap(lhs.firstname, rhs.firstname);
-        swap(lhs.lastname, rhs.lastname);
-        swap(lhs.address, rhs.address);
-        swap(lhs.city, rhs.city);
-        swap(lhs.state, rhs.state);
-        swap(lhs.zipcode, rhs.zipcode);
+    CustomerInfo::CustomerInfo(std::string first, std::string last, std::string address, 
+                                std::string city, std::string state, std::string zip) 
+    {
+        this->firstname = first;
+        this->lastname = last;
+        this->address = address;
+        this->city = city;
+        this->state = state;
+        this->zipcode = zip;
     }
 
-    std::string printInfo();
-};
+    //Copy Constructor
+    CustomerInfo::CustomerInfo(const CustomerInfo &value) 
+    {
+        this->firstname = value.firstname;
+        this->lastname = value.lastname;
+        this->address = value.address;
+        this->city = value.city;
+        this->state = value.state;
+        this->zipcode = value.zipcode;
+    }
 
-//Constructor
-CustomerInfo::CustomerInfo(std::string first, std::string last, std::string address, 
-        std::string city, std::string state, std::string zip) {
-    this->firstname = first;
-    this->lastname = last;
-    this->address = address;
-    this->city = city;
-    this->state = state;
-    this->zipcode = zip;
-}
-
-//Copy Constructor
-CustomerInfo::CustomerInfo(const CustomerInfo &value) {
-    this->firstname = value.firstname;
-    this->lastname = value.lastname;
-    this->address = value.address;
-    this->city = value.city;
-    this->state = value.state;
-    this->zipcode = value.zipcode;
-}
-
-//Destructor
-CustomerInfo::~CustomerInfo() {
-    std::cout << "Destructor Called\n";
-}
+    //Destructor
+    CustomerInfo::~CustomerInfo() 
+    {
+        std::cout << "Destructor Called\n";
+    }
 
 #endif
