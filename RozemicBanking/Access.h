f14514e7d2567220f224cbdc898d70d2be469ec7
Dixon <59415488+radixon@users.noreply.github.com>
2021-02-06 00:42:57 -0500

    Add files via upload
    This is what I have complete at 12:41 am on 02/06/2021.
    
    I didn't add all of the functionality but the base is complete.  Deposit/Withdraw/Display.

---
+++ RozemicBanking/Access.h
@@ -0,0 +1,132 @@
+#ifndef Access
+#define Access
+
+	#include <iostream>
+	#include <string>
+
+	
+	class UserAccount
+	{
+		private:
+						
+		protected:
+			double checkingBalance;
+			double savingsBalance;
+			double money;
+			
+			
+		public:
+			virtual double Depositor(double deposit) {};
+			virtual double Withdrawl(double withdraw) {};
+			virtual void Display() = 0;
+			virtual void note() = 0; 
+			UserAccount(double initialBalance) { money = initialBalance; } 
+		
+			virtual ~UserAccount() = default;
+			UserAccount() = default;
+			UserAccount& operator=(UserAccount&& value) 
+			{ 
+				money = std::move(value.money);
+				return*this;
+			} 
+		
+			UserAccount& operator=(const UserAccount&) {return*this;}
+	}; 
+	
+	class Checking : public UserAccount
+	{ 
+		public: 
+				Checking(double initialBalance) : UserAccount(initialBalance){checkingBalance = money;	};	
+				void note() override
+				{	
+					std::cout << "Checking Account Balance: " << checkingBalance << '\n'; 
+				}
+			
+			Checking& operator=(Checking&&) = default; 
+			Checking& operator=(const Checking&) = default;
+		
+			~Checking() {}; 
+			
+			double Depositor(double deposit) override {
+				checkingBalance = checkingBalance + deposit;
+				return checkingBalance;
+			} 
+			
+			double Withdrawl(double withdraw) override {
+				double temp = checkingBalance;
+				checkingBalance = checkingBalance - withdraw;
+				if(checkingBalance < 0 && checkingBalance > - 5)
+				{
+					std::cout << "Your Checking Is Below $0.00" << "\n";
+					return checkingBalance;
+				}
+				else if(checkingBalance < -5 && checkingBalance > -50)
+				{
+					std::cout << "Your Checking Is Below $(5.00)" << "\n";
+					std::cout << "As A Result Your Account Will Incur a $15.00 Charge." << "\n";
+					checkingBalance = checkingBalance - 15;
+					return checkingBalance;
+				}
+				else if(checkingBalance < -50)
+				{
+					std::cout << "Insufficient Funds." << "\n";
+					checkingBalance = temp;
+					return temp;
+				}
+				else
+				{
+					return checkingBalance;
+				}
+			}
+			
+			void Display() override {
+				std::cout << "Checking Balance: " << checkingBalance << "\n";
+			}
+	}; 
+	
+	
+	class Savings : public UserAccount
+	{ 
+		public: 
+				Savings(double initialBalance) : UserAccount(initialBalance){ savingsBalance = money; };	
+				void note() override
+				{	
+					std::cout << "Savings Account Balance: " << savingsBalance << "\n"; 
+				}
+			
+			Savings& operator=(Savings&&) = default; 
+			Savings& operator=(const Savings&) = default;
+		
+			~Savings() {}; 
+			
+			double Depositor(double deposit) override {
+				savingsBalance = savingsBalance + deposit;
+				return savingsBalance;
+			} 
+			
+			double Withdrawl(double withdraw) override {
+				double temp = savingsBalance;
+				savingsBalance = savingsBalance - withdraw;
+				/*
+				Savings accounts are not allowed to go under $0.00
+				*/
+				if(savingsBalance < 0.00)
+				{
+					std::cout << "Insufficient Funds." << "\n";
+					savingsBalance = temp;
+					return savingsBalance;
+				}
+				else
+				{
+					return savingsBalance;
+				}
+			}
+			
+			void Display() override {
+				std::cout << "Savings Balance: " << savingsBalance << "\n";
+			}
+	}; 
+	
+
+
+#endif
--- RozemicBanking/CustomerInfo.cpp
+++ RozemicBanking/CustomerInfo.cpp
@@ -1,27 +1,119 @@
-
-#include <iostream>
-#include <string>
-#include "CustomerInfo.h"
+	#include <iostream>
+	#include <string>
+	#include <array>
+	#include "CustomerInfo.h"
+	#include "Access.h"
 	
-using namespace std;
+	using namespace std;
 
-//Overloaded Assignment
-CustomerInfo &CustomerInfo::operator=(CustomerInfo &unknown) {
-    swap(*this, unknown);
-    return *this;
-}
+	
+	/***************************************************************
+	<array> was chosen due to the input requirements being the same
+	for all users.
+	****************************************************************/
 
-string CustomerInfo::printInfo() {
-	return "First Name:\t" + this->firstname + "\n"
-		+ "Last Name:\t" + this->lastname + "\n"
-    	+ "Address:\t" + this->address + "\n"
-    	+ "City:\t\t" + this->city + "\n"
-    	+ "State:\t\t" + this->state + "\n"
-    	+ "Zip Code:\t" + this->zipcode + "\n";
-}
 
-// int main()
-// {
-// 	CustomerInfo addcustomer("John", "Doe","3001 Mercer University Dr", "Atlanta", "GA", "30341");
-// 	std::cout << addcustomer << '\n';
-// }
+	std::array<std::string,6> customerHome(std::string firstname="N/A",std::string lastname="N/A",std::string address="N/A",std::string city="N/A",std::string state="N/A",std::string zip="N/A")
+	{
+		/*************************************************************
+		This function will be used to input user information.
+		Defaulting to N/A will allow searches for incomplete entries.
+		**************************************************************/
+		std::string input1;
+		if (firstname == "N/A")
+		{
+			std::cout << "Enter First Name:\n";
+			std::cin >> input1;
+			firstname = input1;
+		}		
+
+
+		if (lastname == "N/A")
+		{
+			std::cout << "Enter Last Name:\n";
+			std::cin >> input1;
+			lastname = input1;
+		}
+
+		if (address == "N/A")
+		{
+			std::cout << "Enter Address:\n";
+			/***************************************************
+			Use getline to allow input that will be more than a 
+			word long.
+			****************************************************/
+			std::getline(std::cin,input1);
+			std::cin.ignore();
+			address = input1;
+		}
+
+		if (city == "N/A")
+		{
+			std::cout << "Enter City:\n";
+			std::cin >> input1;
+			city = input1;
+		}
+
+		if (state == "N/A")
+		{
+			std::cout << "Enter State:\n";
+			std::cin >> input1;
+			state = input1;
+		}
+
+		if (zip == "N/A")
+		{
+			std::cout << "Enter Zip Code:\n";
+			std::cin >> input1;
+			zip = input1;
+		}
+
+		std::array<std::string,6> newcustomer{firstname,lastname,address,city,state,zip};
+
+		return newcustomer;
+
+	}
+
+
+	int main(void)
+	{
+		std::array<std::string,6> customer;
+		double initBalance;
+
+
+		customer = customerHome("John","Doe","3301 Mercer Dr.","Macon","GA","31098");
+		
+		cout << "How Much Money Will You Use To Open Your Checking Account?" << endl;
+		cin >> initBalance;	
+		//caccount = checking account	
+		Checking caccount(initBalance);
+		
+		cout << "How Much Money Will You Use To Open Your Savings Account?" << endl;
+		cin >> initBalance;
+		//saccount = savings account
+		Savings saccount(initBalance);
+		
+		/*
+		account1 points to the checking account.
+		account2 points to the savings account.
+		*/
+		UserAccount *account1 = &caccount;
+		UserAccount *account2 = &saccount;
+		account1->Depositor(125);
+		account2->Withdrawl(125);
+		
+		account1->Display();
+		account2->Display();
+		
+		account1->Withdrawl(500);
+		account2->Depositor(500);
+		
+		account1->Display();
+		account2->Display();
+
+		CustomerInfo newCustomer(customer[0],customer[1],customer[2],customer[3],customer[4],customer[5]);
+		std::cout << newCustomer << "\n";
+		
+
+		return 0;
+	}
