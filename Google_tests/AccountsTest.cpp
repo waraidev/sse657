c9265bfef557e110d3b0af75be454d4ada058995
Emma Anderson <emmagination97@gmail.com>
2021-03-17 16:25:58 -0400

    Added more unit tests

--- Google_tests/AccountsTest.cpp
+++ Google_tests/AccountsTest.cpp
@@ -16,8 +16,10 @@ protected:
     virtual void SetUp() {
         userAccount = new UserAccounts();
         bankAccount = new BankAccount();
+
         bankAccount->deposit(90);
         initAccounts(userAccount, 90);
+        bankAccount->setTransactionLimit(50);
     }
 
     virtual void TearDown() {
@@ -31,20 +33,34 @@ protected:
 
 TEST_F(BankAccountFixture, BankBalanceTest) {
     EXPECT_EQ(bankAccount->getBalance(), 90);
-    EXPECT_EQ(userAccount->getChecking().getBalance(), 90);
+    EXPECT_EQ(checkingBalance(userAccount), 90);
 }
 
 TEST_F(BankAccountFixture, TransactionLimitTest) {
 
-    userAccount->getChecking().setTransactionLimit(50);
     EXPECT_TRUE(userAccount->getChecking().checkLimit(49));
     EXPECT_TRUE(userAccount->getChecking().checkLimit(50));
     EXPECT_FALSE(userAccount->getChecking().checkLimit(51));
-
 }
 
-//TEST(BankAccountTest, ExampleBalance){
-//    BankAccount bankAccount;
-//    bankAccount.deposit(90);
-//    EXPECT_EQ(bankAccount.getBalance(), 90);
-//}
+TEST_F(BankAccountFixture, WithdrawTest) {
+    userAccount->withdraw(49);
+    EXPECT_EQ(userAccount->getChecking().getBalance(), 41);
+
+    // Transaction limit exceeded; no money gets withdrawn
+    userAccount->withdraw(2);
+    EXPECT_EQ(userAccount->getChecking().getBalance(), 41);
+}
+
+TEST_F(BankAccountFixture, TransferTest) {
+    // Transfer from checking to savings
+    userAccount->transfer('C', 'S', 40);
+    EXPECT_EQ(userAccount->getChecking().getBalance(), 50);
+    EXPECT_EQ(userAccount->getSavings().getBalance(), 40);
+
+    // Transfer from savings to checking
+    userAccount->transfer('S', 'C', 10);
+    EXPECT_EQ(userAccount->getChecking().getBalance(), 60);
+    EXPECT_EQ(userAccount->getSavings().getBalance(), 30);
+}
+
